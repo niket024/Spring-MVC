29a6b263fa6f63c047768b282248b7d0ec0414b1
Niket kumar <niketkumar18@gmail.com>
2020-11-19 09:04:32 +0530

    hello servlet

--- HelloMVC/src/main/java/com/nik/controller/HelloController.java
+++ HelloMVC/src/main/java/com/nik/controller/HelloController.java
@@ -2,25 +2,26 @@ package com.nik.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloController {
 
-	@RequestMapping("/hello")
+	@RequestMapping(value ="/hello", method=RequestMethod.GET)
 	public ModelAndView greetings() {
 		System.out.println("I am from controller");
-		ModelAndView modelAndView = new ModelAndView("hello.jsp");
+		ModelAndView modelAndView = new ModelAndView("hello");
 		modelAndView.addObject("key", "Welcome Spring MVC");
 		return modelAndView;
 
 	}
-	@RequestMapping("/welcome")
+	/*@RequestMapping("/welcome")
 	public ModelAndView welcome() {
 		System.out.println("I am from controller welcome");
 		ModelAndView modelAndView = new ModelAndView("hello.jsp");
 		modelAndView.addObject("key", "Welcome Spring MVC");
 		return modelAndView;
 		
-	}
+	}*/
 }
--- HelloMVC/src/main/webapp/index.jsp
+++ HelloMVC/src/main/webapp/index.jsp
@@ -2,5 +2,7 @@
 <body>
 	<h2>Hello World!</h2>
 	<a href="hello">Click here</a>
+	<a href="login.jsp">For login click here</a>
+
 </body>
 </html>
